5c7229a4fb7b1b3b35aaa40f6ea1dfb5357a8776
Suraj <sksuraj148333@gmail.com>
2021-07-14 21:55:13 +0530

    rf first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [getMessage, setGetMessage] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/flask/hello")
+      .then((response) => {
+        console.log("success");
+        setGetMessage(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {getMessage.status === 200 ? (
+          <h3> {getMessage.data.message}</h3>
+        ) : (
+          <h3> Loading</h3>
+        )}
+      </div>
     </div>
   );
 }
